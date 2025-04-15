e02057b3b91505834c2fbf8517185ffba5f1e789
Clayton Leonan <Clayton.leonan@gmail.com>
2025-04-15 14:13:39 -0300

    Atualização no projeto ContaTerminarl

--- projeto-conta-banco/src/ContaTerminal.java
+++ projeto-conta-banco/src/ContaTerminal.java
@@ -4,24 +4,48 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         int conta;
+        int opcao;
         String agencia;
-        String nomeCliente;
+        String nomeCliente = "Roberto";
         Double saldo = 1500.00;
+        int contador = 0;
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===================================\n");
+        System.out.println("*** Bem Vindo ao Banco xxxxx ***");
+        System.out.println("===================================\n");
         
-        System.out.println("Por favor, digite sua AG: ");
-        agencia = scanner.next();
+        
+        while (contador < 1) {
+            System.out.println("Olá " + nomeCliente + ", digite um numero para avançar:\n");
+            System.out.println("1. Ver saldo\n2. Sacar valor\n3. Sair ");
+            opcao = scanner.nextInt();
 
-        System.out.println("Por favor, digite sua conta: ");
-        conta = scanner.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Seu saldo é " + saldo);
+                    break;
+                case 2:
+                    System.out.println("Digite o valor para sacar");
+                    Double valorSolicitado = scanner.nextDouble();
 
-        System.out.println("Por favor, digite seu Nome: ");
-        nomeCliente = scanner.next();
-
-        System.out.println("\"Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque\"");
-      
-
+                    if(valorSolicitado > saldo){
+                        System.out.println("Saldo insuficiente");
+                        System.out.println("===================================\n");
+                    } else {
+                        saldo = saldo - valorSolicitado;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Obrigado por usar o Banco xxxxx!");
+                    contador++;
+                    break;
+                default:
+                    System.out.println("OPÇÃO INVALIDA");
+                    
+            }
+        }
 
     }
 }
